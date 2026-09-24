b9c1221abdb303e1eb10f7114b96d19e38c3f1b5
kumar <kumarajay912282@gmail.com>
2026-09-24 23:01:19 +0530

    Refactor string initialization in S1 class

--- S1.java
+++ S1.java
@@ -1,7 +1,10 @@
 public class S1{
     //initilize the string
     public static void main(String[] args) {
-        String str = "ajay";
+        // String str = "ajay";
+        // System.out.println(str);
+
+        String str = new String("ajay");
         System.out.println(str);
     }
 }
