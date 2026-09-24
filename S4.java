8350f969477541228ed6caec5b2f875aac23326b
kumar <kumarajay912282@gmail.com>
2026-09-24 23:50:47 +0530

    Add initial implementation of S4 class with string operations

---
+++ S4.java
@@ -0,0 +1,15 @@
+public class S4 {
+    //some important keyword
+    public static void main(String[] args) {
+        String str = "My name is Ajay Kumar";
+        System.out.println(str);
+        System.out.println(str.length());
+       System.out.println(str.isEmpty());
+        System.out.println(str.isBlank());
+         System.out.println(str.toUpperCase());
+         System.out.println(str.toLowerCase());
+         System.out.println(str.contains("Ajay"));
+         System.out.println(str.substring(3,6));
+        
+    }
+}
