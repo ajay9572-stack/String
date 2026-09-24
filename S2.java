0d463de5f5835d058577f8855ad387131a38f2b8
kumar <kumarajay912282@gmail.com>
2026-09-24 23:07:25 +0530

    Add initial implementation of S2 class with main method

---
+++ S2.java
@@ -0,0 +1,9 @@
+public class S2 {
+    public static void main(String[] args) {
+        String str = new String("Ajay");
+        System.out.println(str);
+        System.out.println(str.length());
+        System.out.println( str.charAt(1));
+        
+    }
+}
