395f92acab48b9f13e96c0db0398ca7e7e3f1462
kumar <kumarajay912282@gmail.com>
2026-09-25 19:09:07 +0530

    Remove unnecessary blank line in S6 class main method

--- S6.java
+++ S6.java
@@ -2,8 +2,7 @@ public class S6 {
     //count length of string without length()
     public static void main(String[] args) {
         String str = "Ajay";
-        System.out.println(lengthOfString(str));
-        
+        System.out.println(lengthOfString(str));  
     }
     static int lengthOfString(String str){
         int count  = 0;
