d165501f950dc164c9eb71de7e5dd51cb3b6d0ab
kumar <kumarajay912282@gmail.com>
2026-09-25 18:31:58 +0530

    Add initial implementation of S6 class to count string length

---
+++ S6.java
@@ -0,0 +1,15 @@
+public class S6 {
+    //count length of string without length()
+    public static void main(String[] args) {
+        String str = "Ajay";
+        System.out.println(lengthOfString(str));
+        
+    }
+    static int lengthOfString(String str){
+        int count  = 0;
+        for(char ch : str.toCharArray()){
+            count++;
+        }
+        return count;
+    }
+}
